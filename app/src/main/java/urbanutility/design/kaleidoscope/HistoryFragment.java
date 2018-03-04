facf5c55af366ac9a9dcbfac825a8b3ae008c640
Kevin Hsu <jeryen.hsu@gmail.com>
2018-03-03 17:57:52 -0800

    refactor: Implement sample QR reader classes

--- app/src/main/java/urbanutility/design/kaleidoscope/HistoryFragment.java
+++ app/src/main/java/urbanutility/design/kaleidoscope/HistoryFragment.java
@@ -4,18 +4,25 @@ package urbanutility.design.kaleidoscope;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
 
 import java.util.HashSet;
 import java.util.List;
@@ -25,6 +32,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.observers.DisposableSingleObserver;
+import urbanutility.design.kaleidoscope.barcode.BarcodeCaptureActivity;
 import urbanutility.design.kaleidoscope.client.KaleidoService;
 import urbanutility.design.kaleidoscope.model.KaleidoBalance;
 import urbanutility.design.kaleidoscope.model.KaleidoDeposits;
@@ -47,13 +55,20 @@ public class HistoryFragment extends Fragment implements ExchangeListAdapter.Exc
     TextView syncAllButton;
     @BindView(R.id.recyclerExchangeList)
     RecyclerView recyclerExchangeList;
+    @BindView(R.id.public_key_box)
+    EditText publicKeyBox;
+    @BindView(R.id.secret_key_box)
+    EditText secretKeyBox;
+    @BindView(R.id.)
 
     public KaleidoViewModel kaleidoViewModel;
     private OrdersAdapter ordersAdapter;
     private KaleidoService kaleidoService;
-    private String TAG = HistoryFragment.class.getName();
+    private static final String TAG = HistoryFragment.class.getName();
     private SharedPreferences sharedPreferences;
     private ExchangeListAdapter exchangeListAdapter;
+
+    private static final int RC_BARCODE_CAPTURE = 9001;
 
     public HistoryFragment() {// Required empty public constructor
     }
@@ -148,6 +163,7 @@ public class HistoryFragment extends Fragment implements ExchangeListAdapter.Exc
             }
         };
     }
+
     private DisposableSingleObserver<List<KaleidoBalance>> disposableBalancesSingleObserver() {
         return new DisposableSingleObserver<List<KaleidoBalance>>() {
             @Override
@@ -161,6 +177,7 @@ public class HistoryFragment extends Fragment implements ExchangeListAdapter.Exc
             }
         };
     }
+
     private DisposableSingleObserver<List<KaleidoDeposits>> disposableDepositsSingleObserver() {
         return new DisposableSingleObserver<List<KaleidoDeposits>>() {
             @Override
@@ -173,6 +190,43 @@ public class HistoryFragment extends Fragment implements ExchangeListAdapter.Exc
                 e.printStackTrace();
             }
         };
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RC_BARCODE_CAPTURE) {
+            if (resultCode == CommonStatusCodes.SUCCESS) {
+                if (data != null) {
+                    Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
+                    publicKeyBox.setText(barcode.displayValue);
+                    Log.d(TAG, "Barcode read: " + barcode.displayValue);
+                } else {
+                    Toast.makeText(getContext(), "No barcode captured, intent data is null", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "No barcode captured, intent data is null");
+                }
+            } else {
+                Toast.makeText(getContext(), CommonStatusCodes.getStatusCodeString(resultCode), Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    public class ViewPagerAdapter extends PagerAdapter{
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return false;
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            return super.instantiateItem(container, position);
+        }
     }
 }
 
